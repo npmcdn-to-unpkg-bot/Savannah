64387280cb81c4e949a9527e4e5c58441907710d
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-04-23 09:27:00 -0400

    Add category and sorting names for hardware

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('products', {title: 'Savannah'});
+  res.render('products', {title: 'Products'});
 });
 
 module.exports = router;
