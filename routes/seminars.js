d7e78aa2cef1ed6de26453b3a6911a21c4d30e00
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-04-25 11:01:22 -0400

    Mark-up /seminars

--- routes/seminars.js
+++ routes/seminars.js
@@ -7,12 +7,49 @@ router.get('/', (req, res, next) => {
   let viewData = {
     title: 'Seminars',
     seminars: [{
+      id: 0,
       title: "Getting to Know Ruby on Rails",
       description: "Get to know other Rubyists this weekend at a hackathon in our Manhattan development facilities. Whether you’re a n00b or a professional Ruby developer, we’d love to meet you!",
-      header_image_filename: "red_trains.jpg"
-    }]
+      headerImageFilename: "red_trains.jpg"
+    }],
+    filters: [
+      'Upcoming',
+      'Android',
+      'AngularJS',
+      'Backbone',
+      'Back-End',
+      'CoffeeScript',
+      'Django',
+      'Docker',
+      'EmberJS',
+      'ExpressJS',
+      'Far Cry',
+      'Final Fantasy',
+      'Firewatch',
+      'Front-End',
+      'Git',
+      'Grand Theft Auto',
+      'Heroku',
+      'iOS Development',
+      'Laravel',
+      'Less',
+      'Linux',
+      'Minecraft',
+      'Node',
+      'OS X Development',
+      'React',
+      'React Native',
+      'Ruby on Rails',
+      'Task Runners',
+      'tvOS Development',
+      'Ubuntu Server',
+      'Sass',
+      'watchOS Development',
+      'WordPress',
+      'WSGI'
+    ]
   };
-  res.send('seminars', viewData);
+  res.render('seminars', viewData);
 });
 
 module.exports = router;
