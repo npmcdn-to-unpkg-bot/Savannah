0e7387ce30ac63926426b377002fdf0e7a721348
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-05-24 07:53:08 -0400

    Fix cart addition bug

--- routes/cart.js
+++ routes/cart.js
@@ -8,11 +8,14 @@ var User = require('../models/User');
 router.get('/', (req, res, next) => {
   let viewData = {title: 'Cart'};
   if (req.user) {
+    console.log('Getting cart data...');
     products.with_many_asins(req.user.cart).then((values) => {
+      console.log('Done:', values);
       // Move the product data up a level
       for (var i=0; i<values.length; i++) {
         values[i] = values[i][0];
       }
+      console.log('values.length:', values.length);
       viewData.products = values;
       res.render('cart', viewData);
     });
@@ -27,10 +30,13 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/add/:asin', (req, res) => {
+  req.user.cart = (req.user.cart.length) ? req.user.cart : [];
+  req.user.cart.push(req.params.asin);
+  console.log('req.user.cart:', req.user.cart);
   if (req.user) {
     // Remember that the user wants to buy this item
     User.findByIdAndUpdate(req.user._id, {$set: {
-      cart: (req.user.cart = req.user.cart || []).push(req.params.asin)
+      cart: req.user.cart
     }}, (err, user) => {
       if (err) throw err;
       res.redirect('/cart');
